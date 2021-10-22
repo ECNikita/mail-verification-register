9aa2860aa03fd076a68832301765ba84a70c2917
ECNikita <Nikita@ecinfosolutions.com>
2021-10-22 17:01:14 +0530

    changes

--- django_email-main/accounts/databaseProvider/userCRUD.py
+++ django_email-main/accounts/databaseProvider/userCRUD.py
@@ -1,14 +1,15 @@
 from django.db import connection
+from accounts.models import Profile
 
 def Validate_user(email,password):
     cursor = connection.cursor()
     
     try:
-        cursor.execute('SELECT "Email","IsActive","uid" FROM public."User" WHERE public."User"."Email" = %s and public."User"."Password"=%s ',[email,password])
+        cursor.execute('SELECT "Email","Password","IsActive" FROM public."User" WHERE public."User"."Email" = %s and public."User"."Password"=%s ',[email,password])
         
         result_set = cursor.fetchall()
         for row in result_set:
-            return {"Email":row[0],"IsActive":row[1],"uid":row[2]}
+            return {"Email":row[0],"Password":row[1],"IsActive":row[2]}
     finally:
         cursor.close()
     return None
@@ -65,6 +66,63 @@ def Check_user_already_register(email):
         result_set = cursor.fetchall()
         print(result_set)
         for row in result_set:
+            return True
+    finally:
+        cursor.close()
+    return False
+
+def user_all():
+    cursor = connection.cursor()
+    user_all = [] 
+    try:
+        cursor.execute('SELECT uid, "Email", "Password", "IsActive", otp FROM public."User"')
+        
+        result_set = cursor.fetchall()
+        for row in result_set:
+            users =Profile(row[0],row[1],row[2],row[3],row[4])
+            user_all.append(users)
+    finally:
+        cursor.close()
+    return user_all
+
+def user_delete(uid):
+    cursor = connection.cursor()
+    try:
+        cursor.execute('DELETE FROM public."User" WHERE uid = %s',[uid])
+        connection.commit()
+        count = cursor.rowcount
+        if count > 0:
+            return True
+    finally:
+        cursor.close()
+    return False
+
+def user_update(data):
+    cursor = connection.cursor()
+    
+    try:
+        updatedata = [data["uid"],data["Email"],data["Password"]]
+        cursor.execute('UPDATE public."User" SET uid=%s, "Email"=%s, "Password"=%s WHERE uid =%s;',updatedata)
+
+        
+        connection.commit()
+        count = cursor.rowcount
+        if count > 0:
+            return True
+    finally:
+        cursor.close()
+    return False
+
+def user_insert(data):
+    cursor = connection.cursor()
+    
+    try:
+        update =[data["uid"],data["Email"],data["Password"]]
+        cursor.execute('INSERT INTO public."User"(uid, "Email", "Password") VALUES (%s, %s, %s);',update)
+        
+        connection.commit()
+        count = cursor.rowcount
+        if count > 0:
             return True
     finally:
         cursor.close()
--- django_email-main/accounts/views.py
+++ django_email-main/accounts/views.py
@@ -3,7 +3,14 @@ from accounts.models import Profile
 from django.shortcuts import redirect, render
 from accounts.databaseProvider.userCRUD import *
 from django.contrib import messages
-#from .models import *
+from rest_framework.parsers import JSONParser 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from accounts.serialize import AccountsSerialize
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+import json
 
 from django.conf import settings
 from django.core.mail import send_mail
@@ -11,88 +18,137 @@ from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required
-def home(request):
-    return render(request , 'home.html')
-    return redirect('/saveapp')
+@api_view(['GET', 'POST', 'DELETE'])
+def user_list(request):
+    if request.method == 'GET':
+        users = user_all()
+        
+        uids = request.query_params.get('uid', None)
+        
+        if uids is not None:
+            for x in users:     
+                if int(x.uid) == int(uids):
+                    users=[x]
+                    break
+
+        AccountsSerializer = AccountsSerialize(users, many=True)
+        return JsonResponse(AccountsSerializer.data, safe=False)
+
+
+    elif request.method == 'POST':
+        user_data = json.loads(request.body)
+        print(register_data)
+        valid_user = Validate_user_details(user_data["uid"])
+        res = False
+        if valid_user is not None and int(valid_user)==int(user_data["uid"]):
+            res= user_update(user_data)
+        else:
+            res = user_insert(user_data)
+        
+        if res is True:
+            data= {"res":"True"}
+            return JsonResponse((data), safe=False)
+        else :
+            data= {"res":"False"}
+            return JsonResponse((data), status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        uid = request.query_params.get('uid', None)
+        
+        res=False
+        res = user_delete(uid)
+
+        if res is True:
+            data= {"res":"True"}
+            return JsonResponse((data), safe=False)
+        else :
+            data= {"res":"False"}
+            return JsonResponse((data), status=status.HTTP_400_BAD_REQUEST)
 
 
 
-def login_attempt(request):
-    if request.method == 'POST':
-        email = request.POST.get('username')
-        password = request.POST.get('password')
-        user_obj = Validate_user(email,password)
-        print(user_obj)
+# @login_required
+# def home(request):
+#     return render(request , 'home.html')
+#     return redirect('/saveapp')
+
+
+
+# def login_attempt(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user_obj = Validate_user(email,password)
+#         print(user_obj)
         
 
-        if user_obj is None:
-            messages.success(request, 'Email or Password is Wrong.')
-            return redirect('/accounts/login')
+#         if user_obj is None:
+#             messages.success(request, 'Email or Password is Wrong.')
+#             return redirect('/accounts/login')
         
-        print(user_obj)
-        if user_obj["IsActive"] == False:
-            messages.success(request, 'Profile is not activated.')
-            return redirect('/accounts/login')
+#         print(user_obj)
+#         if user_obj["IsActive"] == False:
+#             messages.success(request, 'Profile is not activated.')
+#             return redirect('/accounts/login')
         
-        request.session['uid'] = user_obj["uid"]
-        return redirect('/saveapp/insertapp')
+#         request.session['uid'] = user_obj["uid"]
+#         return redirect('/saveapp/insertapp')
 
-    return render(request , 'login.html')
+#     return render(request , 'login.html')
 
-def register_attempt(request):
+# def register_attempt(request):
 
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        print(password)
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#         print(password)
 
-        try:
-            if Check_user_already_register(email):
-                messages.success(request, 'Email is already taken.')
-                return redirect('/register')
+#         try:
+#             if Check_user_already_register(email):
+#                 messages.success(request, 'Email is already taken.')
+#                 return redirect('/register')
 
             
-            auth_token = random.randint(10000,99999)
-            Register_user(email,password,auth_token)
+#             auth_token = random.randint(10000,99999)
+#             Register_user(email,password,auth_token)
 
-            send_mail_after_registration(email , auth_token)
-            return redirect('/token')
+#             send_mail_after_registration(email , auth_token)
+#             return redirect('/token')
 
-        except Exception as e:
-            print(e)
-
-
-    return render(request , 'register.html')
-
-def success(request):
-    return render(request , 'success.html')
+#         except Exception as e:
+#             print(e)
 
 
+#     return render(request , 'register.html')
 
-def token_send(request):
-    return render(request , 'token_send.html')
+# def success(request):
+#     return render(request , 'success.html')
 
 
 
-def verifyotp(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        otp = request.POST.get('otp')
+# def token_send(request):
+#     return render(request , 'token_send.html')
+
+
+
+# def verifyotp(request):
+#     if request.method == 'POST':
+#         email = request.POST.get('email')
+#         otp = request.POST.get('otp')
     
-        if Verify_user(email,otp) == False:
-            messages.success(request, 'Email or OTP is Wrong.')
-            return redirect('/accounts/verifyotp')
+#         if Verify_user(email,otp) == False:
+#             messages.success(request, 'Email or OTP is Wrong.')
+#             return redirect('/accounts/verifyotp')
         
-        Activate_user(email)
-        return redirect('/accounts/login')
+#         Activate_user(email)
+#         return redirect('/accounts/login')
         
     
-    return render(request , 'otp.html')
+#     return render(request , 'otp.html')
 
 
-def error_page(request):
-    return  render(request , 'error.html')
+# def error_page(request):
+#     return  render(request , 'error.html')
 
 
 
